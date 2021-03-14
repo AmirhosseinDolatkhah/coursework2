dc1aa4a20339dbb82928d6b06907caceff41e68a
AHDScience <ahdscience.ce@gmail.com>
2021-03-14 18:25:13 +0330

    Add MainFrame

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,20 @@
 package controller;
 
-public class Controller {
+import model.DataFrame;
+import view.WindowFrame;
+
+import javax.swing.*;
+
+public final class Controller {
+    private final static WindowFrame windowFrame;
+    private final static DataFrame dataFrame;
+
+    static {
+        windowFrame = new WindowFrame();
+        dataFrame = new DataFrame();
+    }
+
+    public static void run() {
+        SwingUtilities.invokeLater(windowFrame);
+    }
 }
